1482b1555beef28c2bf4b916c2df55665cfade90
longhe.li <lilonghe@outlook.com>
2023-08-27 21:32:04 -0500

    update gap size

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Card from "../components/card"
 
 export default function Index() {
     return (
-        <div className="m-[20px] flex gap-3 flex-wrap">
+        <div className="m-[20px] flex gap-5 flex-wrap">
             {data.map(cate => <Card.Group key={cate.name} data={cate}  />)}
         </div>
     )
--- pages/solutions/index.jsx
+++ pages/solutions/index.jsx
@@ -3,7 +3,7 @@ import Card from '../../components/card'
 
 export default function Solutions () {
   return (
-    <div className='m-[20px] flex gap-3 flex-wrap'>
+    <div className='m-[20px] flex gap-5 flex-wrap'>
       <div className="flex wrap gap-4">
         {solutionList.map(cate => <Card.Group key={cate.name} data={cate}  />)}
       </div>
